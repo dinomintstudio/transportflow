389b46db45cdb4f3214e650096ef7723fd9d8b16
ivanjermakov <vania6600@gmail.com>
2020-01-14 21:36:47 +0300

    matcher service

---
+++ src/app/util/service/matcher.service.ts
@@ -0,0 +1,16 @@
+import {Injectable} from '@angular/core';
+import {Maybe} from "../../common/model/Maybe";
+
+@Injectable({
+	providedIn: 'root'
+})
+export class MatcherService {
+
+	constructor() {
+	}
+
+	match<K, V>(key: K, cases: Map<K, V>): Maybe<V> {
+		return new Maybe(cases.get(key))
+	}
+
+}
