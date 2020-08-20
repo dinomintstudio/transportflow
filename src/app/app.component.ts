6c86efabb2a29c117957946e06e8ebd89fecb642
ivanjermakov <ivanjermakov1@gmail.com>
2020-08-20 14:03:22 +0300

    World service refactoring

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,12 +14,13 @@ import {RandomService} from './common/service/random.service'
 import {CityGenerationConfig} from './generation/city/config/CityGenerationConfig'
 import {CityGenerationService} from './generation/city/service/city-generation.service'
 import {RenderService} from './render/service/render.service'
-import {WorldService} from './game-logic/service/world.service'
 import {WorldGenerationConfig} from './generation/world/config/WorldGenerationConfig'
 import {DesertBiomeConfig} from './generation/terrain/config/biome/DesertBiomeConfig'
 import {TaigaBiomeConfig} from './generation/terrain/config/biome/TaigaBiomeConfig'
 import {JungleBiomeConfig} from './generation/terrain/config/biome/JungleBiomeConfig'
 import {AltitudeMapConfig} from './generation/terrain/config/noisemap/AltitudeMapConfig'
+import {WorldGenerationService} from './generation/world/service/world-generation.service'
+import {WorldService} from './game-logic/service/world.service'
 
 @Component({
 	selector: 'app-root',
@@ -35,6 +36,7 @@ export class AppComponent {
 		private randomService: RandomService,
 		private cityGenerationService: CityGenerationService,
 		private renderService: RenderService,
+		private worldGenerationService: WorldGenerationService,
 		private worldService: WorldService
 	) {
 		this.randomService.seed.set('seed')
@@ -101,7 +103,7 @@ export class AppComponent {
 		)
 
 		this.worldService.world.set(
-			this.worldService.generate(
+			this.worldGenerationService.generate(
 				tiledTerrain,
 				new WorldGenerationConfig(
 					terrainGenerationConfig,
--- src/app/game-logic/service/world.service.ts
+++ src/app/game-logic/service/world.service.ts
@@ -1,23 +1,15 @@
 import {Injectable} from '@angular/core'
 import {ObservableData} from '../../common/model/ObservableData'
 import {World} from '../model/World'
-import {TiledTerrain} from '../../generation/terrain/model/TiledTerrain'
-import {WorldGenerationConfig} from '../../generation/world/config/WorldGenerationConfig'
-import {Tile} from '../model/Tile'
-import {Maybe} from '../../common/model/Maybe'
-import {CityGenerationService} from '../../generation/city/service/city-generation.service'
-import {TiledCity} from '../../generation/city/model/TiledCity'
-import {Matrix} from '../../common/model/Matrix'
-import {TerrainTile} from '../../generation/terrain/model/TerrainTile'
-import {Position} from '../../common/model/Position'
-import {Road} from '../model/Road'
-import {RoadTile} from '../model/RoadTile'
-import {Building} from '../model/Building'
-import {Rectangle} from '../../common/model/Rectangle'
-import {Shape} from '../../common/model/Shape'
 import {Log} from '../../common/model/Log'
 import {MonoTypeOperatorFunction} from 'rxjs'
+import {Position} from '../../common/model/Position'
 import {withLatestFrom} from 'rxjs/operators'
+import {Matrix} from '../../common/model/Matrix'
+import {Tile} from '../model/Tile'
+import {Maybe} from '../../common/model/Maybe'
+import {Rectangle} from '../../common/model/Rectangle'
+import {Shape} from '../../common/model/Shape'
 
 /**
  * Responsible for world initialization, generation, including terrain, natural structures, city and building placing
@@ -32,39 +24,7 @@ export class WorldService {
 
 	world: ObservableData<World> = new ObservableData<World>()
 
-	constructor(
-		private cityGenerationService: CityGenerationService
-	) {}
-
-	/**
-	 * Generate terrain and natural structures
-	 * @param tiledTerrain
-	 * @param config
-	 */
-	generate(tiledTerrain: TiledTerrain, config: WorldGenerationConfig): World {
-		this.log.debug('generate world')
-		const tilemap: Matrix<Tile> = this.mapTerrainMatrixToTileMatrix(tiledTerrain.tilemap)
-
-		tiledTerrain.cityPoints
-			.filter(p => this.canPlaceCity(tiledTerrain.tilemap, p))
-			.forEach(cityPosition => {
-				const city: TiledCity = this.cityGenerationService.generate(config.cityGenerationConfig)
-				// place cityPoint in the center of city tilemap
-				const worldCityPosition: Position = cityPosition.add(
-					Position
-						.fromShape(city.tilemap.shape.map(c => -c / 2))
-						.floor()
-				)
-
-				this.fillCityTile(city, worldCityPosition, tilemap)
-				this.placeBuildings(city, worldCityPosition, tilemap)
-			})
-
-		return new World(
-			tilemap,
-			config
-		)
-	}
+	constructor() {}
 
 	/**
 	 * Find adjacent tiles to the tile in specified position.
@@ -97,103 +57,6 @@ export class WorldService {
 					: world.tilemap.shape.height + (y % world.tilemap.shape.height),
 			)
 		)
-	}
-
-	private placeBuildings(city: TiledCity, worldCityPosition: Position, tilemap: Matrix<Tile>): void {
-		city.generatedCityTemplate.buildings.forEach((building) => {
-			const worldTilePosition: Position = worldCityPosition.add(building.position.topLeft)
-			building.position = building.position.translate(worldCityPosition)
-			// out of world map range
-			if (!tilemap.has(worldTilePosition)) return
-			const worldTile: Tile = tilemap.at(worldTilePosition)
-
-			const adjacentTileMatrix = this.getAdjacentTileMatrix(tilemap, worldTilePosition)
-			if (!this.canBuild(worldTile)) return
-			if (!this.canBuildBuilding(building, adjacentTileMatrix)) return
-
-			worldTile.isPlant = false
-			worldTile.city = new Maybe<TiledCity>(city)
-			worldTile.building = new Maybe<Building>(building)
-			if (building.position.shape.height === 1) {
-				adjacentTileMatrix.at(new Position(1, 2)).ifPresent(t => t.building = worldTile.building)
-			}
-			if (building.position.shape.width === 1) {
-				adjacentTileMatrix.at(new Position(2, 1)).ifPresent(t => t.building = worldTile.building)
-			}
-			if (building.position.shape.area() === 1) {
-				adjacentTileMatrix.at(new Position(2, 2)).ifPresent(t => t.building = worldTile.building)
-			}
-		})
-	}
-
-	private fillCityTile(city: TiledCity, worldCityPosition: Position, tilemap: Matrix<Tile>): void {
-		city.tilemap.forEach((cityTile, position) => {
-			if (!cityTile.isPresent()) return
-
-			const worldTilePosition: Position = worldCityPosition.add(position)
-			// out of world map range
-			if (!tilemap.has(worldTilePosition)) return
-			const worldTile: Tile = tilemap.at(worldTilePosition)
-
-			if (!this.canBuild(worldTile)) return
-
-			worldTile.isPlant = false
-			worldTile.city = new Maybe<TiledCity>(city)
-
-			if (cityTile.get().type == 'road') {
-				worldTile.road = new Maybe<RoadTile>(new RoadTile(
-					Maybe.empty(),
-					new Maybe(new Road('roadway')),
-					Maybe.empty()
-				))
-			}
-		})
-	}
-
-	private canBuild(tile: Tile): Boolean {
-		return tile.surface.type === 'land' && !tile.road.isPresent() && !tile.building.isPresent()
-	}
-
-	private canBuildBuilding(building: Building, adjacentTiles: Matrix<Maybe<Tile>>): Boolean {
-		const centerTile = adjacentTiles.at(new Position(1, 1))
-		const middleRightTile = adjacentTiles.at(new Position(2, 1))
-		const bottomCenterTile = adjacentTiles.at(new Position(1, 2))
-		const bottomRightTile = adjacentTiles.at(new Position(2, 2))
-
-		if (!this.canBuild(centerTile.get())) return false
-		if (building.position.shape.area() === 1 &&
-			(!bottomRightTile.isPresent() || !this.canBuild(bottomRightTile.get()))
-		) return false
-		if (building.position.shape.width === 1 &&
-			(!middleRightTile.isPresent() || !this.canBuild(middleRightTile.get()))
-		) return false
-		if (building.position.shape.height === 1 &&
-			(!bottomCenterTile.isPresent() || !this.canBuild(bottomCenterTile.get()))
-		) return false
-
-		return true
-	}
-
-	private mapTerrainMatrixToTileMatrix(terrainMatrix: Matrix<TerrainTile>): Matrix<Tile> {
-		return terrainMatrix.map((terrainTile, position) => new Tile(
-			position,
-			terrainTile.surface,
-			terrainTile.biome,
-			terrainTile.isPlant,
-			terrainTile.isSnow,
-			Maybe.empty(),
-			Maybe.empty(),
-			Maybe.empty()
-		))
-	}
-
-	/**
-	 * City can be placed only on land surface
-	 * @param tilemap
-	 * @param position
-	 */
-	private canPlaceCity(tilemap: Matrix<TerrainTile>, position: Position) {
-		return tilemap.has(position) && tilemap.at(position).surface.type === 'land'
 	}
 
 }
